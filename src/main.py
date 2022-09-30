ad0280589bbdaaeddfd34e7f3477dd6caec6b4bd
Haerbernd <105395307+Haerbernd@users.noreply.github.com>
2022-09-30 23:18:07 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 import sys
-import logger as log
+from src import logger as log, utils
 import os
 
 queue = []
@@ -7,30 +7,29 @@ queue = []
 
 def start(config):
     log.main.info('Main program started...')
-    os.system(str(config['drive']))
-    log.debug.debug('Changed drive to ' + str(config['drive']) + '...')
-    os.system('cd ' + str(config['yt-dlp-path']))
-    log.debug.debug('Changed dictionary to ' + str(config['yt-dlp-path']) + '...')
-    os.system('cls')
-    log.debug.debug('Cleaned screen...')
-    video = input('Input video/playlist to download!')
+
+    video = input('Input video/playlist to download!\n')
     queue.append(video)
     os.system('cls')
     log.debug.debug('Cleaned screen...')
+
     while True:
-        video = input('Input video/playlist to download!\n'
-                      'Input !done when all videos/playlists that you wish to download are inputted!')
-        if video == '!quit':
+        video = input('Input video/playlist to download!\n\n'
+                      'Input ' + utils.Color.red + '!done' + utils.Color.reset +
+                      ' when all videos/playlists that you wish to download are inputted!\n')
+        if video == '!done':
             break
         queue.append(video)
         os.system('cls')
         log.debug.debug('Cleaned screen...')
     os.system('cls')
     log.debug.debug('Cleaned screen...')
+
     for i in queue:
         log.main.info('Started downloading a video...')
         log.debug.debug('Started downloading ' + str(i))
-        os.system('.\yt-dlp ' + str(queue[i]))
+        print(config['drive'] + ':&cd ' + config['yt-dlp-path'] + '&.\yt-dlp ' + i)
+        os.system(config['drive'] + ':&cd ' + config['yt-dlp-path'] + '&.\yt-dlp ' + i)
         log.main.info('Finished downloading a video/playlist')
         log.debug.debug('Finished downloading ' + str(i))
         os.system('cls')
